4c115d1801522dff9d1aca7bbe8919c1a6c62957
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 22:45:11 -0300

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tideshandbook';
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.activatedRoute.url.pipe(take(3)).subscribe(res => console.log('Current url: ', res))
+  }
 }
