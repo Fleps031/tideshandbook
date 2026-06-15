c545416d5d08a994af4d880ffb3dc4bf8cfe6161
Thais Gomes Gonçalves <thais@MacBook-Air-de-Thais.local>
2026-06-14 21:27:13 -0300

    config for typeorm

--- backend/handbookserver/src/app.module.ts
+++ backend/handbookserver/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true}),
+    DbModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
